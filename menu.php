62bee66be7b654a41447f0bd52a9eb70b1031003
Karolllg99 <151976045+Karollg99@users.noreply.github.com>
2023-12-16 00:57:20 -0500

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,9 @@
+<nav class="navbar-expand navbar-dark bg-dark mb-5">
+        <ul class='navbar-nav'>
+            <li class="nav-item">
+              <a class= "nav-link" href="<?php echo ROOT?>/candidatos/index.php" > Formulario de Contacto </a>
+              <li class="nav-item">
+              <a class= "nav-link" href="<?= ROOT ?>/candidatos/add.php">Ingresar Datos de Contacto</a>;
+            </li>
+        </ul>
+</nav>
